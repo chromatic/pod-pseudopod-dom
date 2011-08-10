e7594c73d590cc5db7c507857478dda014526ff9
chromatic <chromatic@wgz.org>
2011-08-10 00:25:44 -0700

    Enabled very basic round-tripping of PPod documents.

--- lib/Pod/PseudoPod/DOM.pm
+++ lib/Pod/PseudoPod/DOM.pm
@@ -1,10 +1,202 @@
-package Pod::PseudoPod::DOM
+package Pod::PseudoPod::DOM;
 # ABSTRACT: an object model for Pod::PseudoPod documents
 
 use strict;
 use warnings;
 
 use parent 'Pod::PseudoPod';
+use Pod::PseudoPod::DOM::Elements;
 
+sub new
+{
+    my ($class, %args)      = @_;
+    my $role                = delete $args{formatter_role};
+    my $self                = $class->SUPER::new(@_);
+    $self->{class_registry} = {};
+    $self->{formatter_role} = $role;
+
+    $self->accept_targets( 'html', 'HTML' );
+    $self->accept_targets_as_text(
+        qw( author blockquote comment caution
+            editor epigraph example figure important listing literal note
+            production programlisting screen sidebar table tip warning )
+    );
+
+    $self->nix_X_codes(1);
+    $self->nbsp_for_S(1);
+    $self->codes_in_verbatim(1);
+
+    return $self;
+}
+
+sub get_document
+{
+    my $self = shift;
+    return $self->{Document};
+}
+
+sub make
+{
+    my ($self, $type, @args) = @_;
+    my $registry             = $self->{class_registry};
+    my $class                = $registry->{$type};
+
+    unless ($class)
+    {
+        my $name = 'Pod::PseudoPod::DOM::Element::' . $type;
+        $class   = $registry->{$type}
+                 = $name->with_traits( $self->{formatter_role} );
+    }
+
+    return $class->new( @args );
+}
+
+sub start_Document
+{
+    my $self = shift;
+
+    $self->{active_elements} =
+    [
+        $self->{Document} = $self->make( Document => type => 'document' )
+    ];
+}
+
+sub end_Document
+{
+    my $self = shift;
+    $self->{active_elements} = [];
+}
+
+sub reset_to_document
+{
+    my $self = shift;
+    $self->{active_elements} = [ $self->{Document} ];
+}
+
+sub push_element
+{
+    my $self  = shift;
+    my $child = $self->make( @_ );
+
+    $self->{active_elements}[-1]->add_children( $child );
+    push @{ $self->{active_elements } }, $child;
+}
+
+sub add_element
+{
+    my $self  = shift;
+    my $child = $self->make( @_ );
+    $self->{active_elements}[-1]->add( $child );
+}
+
+sub reset_to_item
+{
+    my ($self, $type, %attributes) = @_;
+    my $elements                   = $self->{active_elements};
+    my $class                      = 'Pod::PseudoPod::DOM::Element::' . $type;
+
+    while (@$elements)
+    {
+        my $element = pop @$elements;
+        next unless $element->isa( $class );
+
+        # reset iterator
+        my $attrs = keys %attributes;
+
+        while (my ($attribute, $value) = each %attributes)
+        {
+            $attrs-- if $element->$attribute() eq $value;
+        }
+
+        return unless $attrs;
+    }
+}
+
+BEGIN
+{
+    for my $heading ( 1 .. 4 )
+    {
+        my $start_meth = sub
+        {
+            my $self = shift;
+            $self->reset_to_document;
+            $self->push_element(
+                Heading => level => $heading, type => 'header'
+            );
+        };
+
+        my $end_meth = sub
+        {
+            my $self = shift;
+            $self->reset_to_item( Heading => level => $heading );
+        };
+
+        do
+        {
+            no strict 'refs';
+            *{ 'start_head' . $heading } = $start_meth;
+            *{ 'end_head'   . $heading } = $end_meth;
+        };
+    }
+
+    my %text_types =
+    (
+        Z => 'Anchor',
+        I => 'Italics',
+    );
+
+    while (my ($tag, $type) = each %text_types)
+    {
+        my $start_meth = sub
+        {
+            my $self = shift;
+            $self->push_element( 'Text::' . $type, type => lc $type );
+        };
+
+        my $end_meth = sub
+        {
+            my $self = shift;
+            $self->reset_to_item( 'Text::' . $type, type => lc $type );
+        };
+
+        do
+        {
+            no strict 'refs';
+            *{ 'start_' . $tag } = $start_meth;
+            *{ 'end_'   . $tag } = $end_meth;
+        };
+    }
+}
+
+sub handle_text
+{
+    my $self = shift;
+    $self->add_element( Text => type => 'text' );
+    $self->add_element( 'Text::Plain' => type => 'text', content => shift );
+}
+
+sub start_Para
+{
+    my $self = shift;
+    $self->push_element( Paragraph => type => 'paragraph' );
+}
+
+sub end_Para
+{
+    my $self = shift;
+    $self->reset_to_item( Paragraph => type => 'paragraph' );
+}
+
+sub start_for
+{
+    my ($self, $flags) = @_;
+    $self->push_element( Block => type => $flags->{target} || 'unknown_block' );
+}
+
+sub end_for
+{
+    my $self = shift;
+    $self->reset_to_item( 'Block' );
+}
 
 1;
---
+++ lib/Pod/PseudoPod/DOM/Elements.pm
@@ -0,0 +1,190 @@
+package Pod::PseudoPod::DOM::Elements;
+# ABSTRACT: the base classes for PseudoPod DOM objects
+
+use strict;
+use warnings;
+
+use Moose;
+
+{
+    package Pod::PseudoPod::DOM::Element;
+
+    use Moose;
+    with 'MooseX::Traits';
+
+    has 'type', is => 'ro', required => 1;
+}
+
+{
+    package Pod::PseudoPod::DOM::ParentElement;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::Element';
+
+    has 'children',
+        is      => 'ro',
+        isa     => 'ArrayRef[Pod::PseudoPod::DOM::Element]',
+        default => sub { [] };
+
+    sub add
+    {
+        my $self = shift;
+        push @{ $self->children }, @_;
+    }
+
+    sub add_children { push @{ shift->children }, @_ }
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Paragraph;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Text;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::Element';
+    has 'content', is => 'rw';
+
+    sub add
+    {
+        my $self = shift;
+        $self->content( shift );
+    }
+}
+
+{
+    my $parent = 'Pod::PseudoPod::DOM::Element::Text';
+
+    for my $text_item (qw(
+        Anchor Bold Code Entity File Footnote Italics Index Link Plain
+        Subscript Superscript URL ))
+    {
+        Class::MOP::Class->create(
+            $parent . '::' . $text_item => superclasses => [ $parent ]
+        );
+    }
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Heading;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::Element';
+
+    has 'level', is => 'ro', required => 1;
+    has 'text',  is => 'rw';
+
+    sub add
+    {
+        my ($self, $item) = @_;
+        $self->text( $item );
+    }
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::List;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::ListItem;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::Element';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Sidebar;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+
+    has 'title', is => 'rw', default => '';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Figure;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::Element';
+
+    has 'file',    is => 'rw', isa => 'Pod::PseudoPod::DOM::Element::File';
+    has 'anchor',  is => 'rw', isa => 'Pod::PsuedoPod::DOM::Element::Anchor';
+    has 'caption', is => 'rw', isa => 'Pod::PsuedoPod::DOM::Element::Text';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Block;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Table;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+
+    sub headrow
+    {
+        my $self = shift;
+        my $rows = $self->children;
+
+        return unless @$rows;
+        return $rows->[0];
+    }
+
+    sub bodyrows
+    {
+        my $self = shift;
+        my $rows = $self->children;
+
+        return unless @$rows and @$rows > 1;
+        return @{ $rows }[1 .. $#$rows ];
+    }
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::TableRow;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+
+    sub cells { shift->children }
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::TableCell;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+}
+
+{
+    package Pod::PseudoPod::DOM::Element::Document;
+
+    use Moose;
+
+    extends 'Pod::PseudoPod::DOM::ParentElement';
+}
+
+1;
