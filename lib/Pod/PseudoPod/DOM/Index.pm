9730d7903d3a0c75d2ae38141604587a58aa4d89
chromatic <chromatic@wgz.org>
2012-03-24 12:08:47 -0700

    Switched to XHTML 1.1 doctype.

--- lib/Pod/PseudoPod/DOM/Index.pm
+++ lib/Pod/PseudoPod/DOM/Index.pm
@@ -39,8 +39,13 @@ sub emit_index
     my $self    = shift;
     my $entries = $self->entries;
     my $heading = <<END_HTML_HEAD;
-<html>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+    "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="../css/style.css" type="text/css" />
 </head>
 <body>
